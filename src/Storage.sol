9e02f0a798cfe8ff4dee256d6bd6bded8a5c41ff
Can Vardar <ismailcanvardar@gmail.com>
2024-07-25 16:21:46 +0300

    feat(Storage): add new getSlot types
    - getSlot function is implemented for address, int256, and uint256 types.

--- src/Storage.sol
+++ src/Storage.sol
@@ -4,6 +4,24 @@ pragma solidity 0.8.26;
 import { Slot } from "./types/Slot.sol";
 
 library Storage {
+    function getSlot(address slot) public pure returns (Slot storage s) {
+        assembly {
+            s.slot := slot
+        }
+    }
+
+    function getSlot(int256 slot) public pure returns (Slot storage s) {
+        assembly {
+            s.slot := slot
+        }
+    }
+
+    function getSlot(uint256 slot) public pure returns (Slot storage s) {
+        assembly {
+            s.slot := slot
+        }
+    }
+
     function getSlot(bytes32 slot) public pure returns (Slot storage s) {
         assembly {
             s.slot := slot
