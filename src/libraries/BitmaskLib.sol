caefb3a325a89d578b2a8cb5c6f1ed275edc0782
Can Vardar <ismailcanvardar@gmail.com>
2024-07-26 13:24:32 +0300

    feat: add BitmaskLib.sol

---
+++ src/libraries/BitmaskLib.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.26;
+
+import { Mask } from "../types/Mask.sol";
+
+library BitmaskLib {
+    function generate(uint256 maskLength) public pure returns (Mask mask) {
+        assembly {
+            mask := not(sub(shl(maskLength, 1), 1))
+        }
+    }
+
+    function generate(uint256 maskLength, uint256 leftShiftLength) public pure returns (Mask mask) {
+        assembly {
+            /// bytes4(keccak256("WrongParameters()"))
+            if gt(maskLength, leftShiftLength) {
+                mstore(0x80, 0xbd28cf5f)
+                revert(0x9c, 0x04)
+            }
+
+            mask := not(shl(leftShiftLength, sub(shl(maskLength, 1), 1)))
+        }
+    }
+}
