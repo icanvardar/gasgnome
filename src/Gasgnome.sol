2cc44be6afc02c730ed20d21cc3c028ede256bd2
Can Vardar <ismailcanvardar@gmail.com>
2024-07-26 22:29:44 +0300

    feat: add ArithmeticLib library

--- src/Gasgnome.sol
+++ src/Gasgnome.sol
@@ -1,8 +1,9 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.26;
 
-import "./libraries/StorageLib.sol";
+import "./libraries/ArithmeticLib.sol";
 import "./libraries/BitmaskLib.sol";
+import "./libraries/StorageLib.sol";
 
 library Gasgnome {
     string internal constant AUTHOR = "https://github.com/icanvardar";
---
+++ test/ArithmeticLib.t.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.26;
+
+import { ArithmeticLib } from "../src/libraries/ArithmeticLib.sol";
+import { Test, console } from "forge-std/Test.sol";
+
+contract ArithmeticLibTest is Test { }
