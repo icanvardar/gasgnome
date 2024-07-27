b7a03eef1676fa83fd8445485e64aac4a2a44ab1
Can Vardar <ismailcanvardar@gmail.com>
2024-07-27 23:03:44 +0300

    feat(SignedInt): add functions
    The functions `convertWithSize` and `sizeInBits` are implemented similarly to those in the `UnsignedIntLib`. There are minor modifications in the sizeInBits function, with the most significant being the identification of whether a given SignedInt parameter is a negative or positive number.

--- src/types/SignedInt.sol
+++ src/types/SignedInt.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.26;
 
+import { ArithmeticLib } from "../libraries/ArithmeticLib.sol";
+
 type SignedInt is bytes32;
 
 using {
@@ -52,5 +54,45 @@ function modSignedInt(SignedInt left, SignedInt right) pure returns (SignedInt r
 function expSignedInt(SignedInt left, SignedInt right) pure returns (SignedInt res) {
     assembly {
         res := exp(left, right)
+    }
+}
+
+using SignedIntLib for SignedInt global;
+
+library SignedIntLib {
+    function convertWithSize(SignedInt u, uint16 sizeInBytes) public pure returns (int256 to) {
+        bytes32 result = ArithmeticLib.convertWithSize(SignedInt.unwrap(u), sizeInBytes, sizeInBits(u));
+
+        assembly {
+            to := result
+        }
+    }
+
+    function sizeInBits(SignedInt s) public pure returns (uint8 size) {
+        assembly {
+            let tmp
+            let isNegative := sgt(0, s)
+            let absValue := s
+            if isNegative { absValue := sub(0, s) }
+
+            for { let i := 0x1f } gt(i, 0x0) { i := sub(i, 1) } {
+                let b := byte(i, absValue)
+
+                /// Check existency of first nibble
+                let x := and(b, 0x0f)
+                if gt(x, 0) {
+                    size := add(size, add(tmp, 0x1))
+                    tmp := 0x0
+                }
+                if eq(x, 0) { tmp := add(tmp, 0x1) }
+
+                /// Check existency of second nibble
+                let y := and(b, 0xf0)
+                if gt(y, 0) { size := add(size, 0x1) }
+                if eq(y, 0) { tmp := add(tmp, 0x1) }
+            }
+
+            size := mul(size, 0x4)
+        }
     }
 }
