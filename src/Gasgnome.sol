72db6f02162941cf5366ccc3fdfc281f1a45f3a0
Can Vardar <ismailcanvardar@gmail.com>
2024-07-27 23:20:23 +0300

    refactor: update Gasgnome.sol

--- src/Gasgnome.sol
+++ src/Gasgnome.sol
@@ -1,9 +1,15 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.26;
 
+/// @notice Libraries
 import "./libraries/ArithmeticLib.sol";
 import "./libraries/BitmaskLib.sol";
 import "./libraries/StorageLib.sol";
+
+/// @notice Types
+import "./types/SignedInt.sol";
+import "./types/Slot.sol";
+import "./types/UnsignedInt.sol";
 
 library Gasgnome {
     string internal constant AUTHOR = "https://github.com/icanvardar";
