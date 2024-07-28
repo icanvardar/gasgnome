62055c2b447dbd4d9df345d0751e6d7245a20e28
Can Vardar <ismailcanvardar@gmail.com>
2024-07-28 04:23:23 +0300

    test(UnsignedIntLibTest): create test suite

---
+++ test/UnsignedIntLib.t.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.26;
+
+import { ArithmeticLib } from "../src/libraries/ArithmeticLib.sol";
+import { Test, console } from "forge-std/Test.sol";
+
+contract UnsignedIntLibTest is Test {
+    function test_ConvertWithSize() public view { }
+
+    function test_SizeInBits() public view { }
+
+    function test_AddUnsignedInt() public view { }
+
+    function test_SubUnsignedInt() public view { }
+
+    function test_MulUnsignedInt() public view { }
+
+    function test_DivUnsignedInt() public view { }
+
+    function test_ModUnsignedInt() public view { }
+
+    function test_ExpUnsignedInt() public view { }
+}
