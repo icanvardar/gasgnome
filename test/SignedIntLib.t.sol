51ab7b5b0bcc0819d56351c243cb30cc9db10ae6
Can Vardar <ismailcanvardar@gmail.com>
2024-07-28 04:23:15 +0300

    test(SignedIntLibTest): create test suite

---
+++ test/SignedIntLib.t.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.26;
+
+import { ArithmeticLib } from "../src/libraries/ArithmeticLib.sol";
+import { Test, console } from "forge-std/Test.sol";
+
+contract SignedIntLibTest is Test {
+    function test_ConvertWithSize() public view { }
+
+    function test_SizeInBits() public view { }
+
+    function test_AddSignedInt() public view { }
+
+    function test_SubSignedInt() public view { }
+
+    function test_MulSignedInt() public view { }
+
+    function test_DivSignedInt() public view { }
+
+    function test_ModSignedInt() public view { }
+
+    function test_ExpSignedInt() public view { }
+}
