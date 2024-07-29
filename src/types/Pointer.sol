bc2fea8f46ab83080f04fcaa29d38c19f382b2e0
Can Vardar <ismailcanvardar@gmail.com>
2024-07-29 13:51:04 +0300

    refactor(Pointer): update function visibility

--- src/types/Pointer.sol
+++ src/types/Pointer.sol
@@ -1,24 +1,26 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.26;
 
+import { MemoryLib } from "../libraries/MemoryLib.sol";
+
 type Pointer is bytes32;
 
 using PointerLib for Pointer global;
 
 library PointerLib {
-    function load(Pointer ptr) public pure returns (bytes32 result) {
+    function load(Pointer ptr) internal pure returns (bytes32 result) {
         assembly {
             result := mload(ptr)
         }
     }
 
-    function next(Pointer ptr) public pure returns (Pointer result) {
+    function next(Pointer ptr) internal pure returns (Pointer result) {
         assembly {
             result := add(ptr, 0x20)
         }
     }
 
-    function store(Pointer ptr, bytes32 data) public pure returns (Pointer nextPtr) {
+    function store(Pointer ptr, bytes32 data) internal pure returns (Pointer nextPtr) {
         assembly {
             mstore(ptr, data)
 
@@ -26,26 +28,25 @@ library PointerLib {
         }
     }
 
-    function store(Pointer ptr, bytes32 data, bool isImmutable) public pure returns (Pointer nextPtr) {
+    function store(Pointer ptr, bytes32 data, bool isImmutable) internal pure returns (Pointer nextPtr) {
         assembly {
             mstore(ptr, data)
 
             nextPtr := add(ptr, 0x20)
         }
 
-        bytes32 msp = memoryStorageLocation(ptr);
+        Pointer msp = memoryStorageLocation(ptr);
 
         assembly {
             if eq(isImmutable, 0x1) { mstore(mload(msp), 0x1) }
         }
     }
 
-    function memoryStorageLocation(Pointer ptr) internal pure returns (bytes32 result) {
-        assembly {
-            let u := 0x6ffffff
-            let r := div(ptr, 0x20)
+    function update(Pointer ptr, bytes32 data) internal pure {
+        MemoryLib.update(Pointer.unwrap(ptr), data);
+    }
 
-            result := sub(ptr, r)
-        }
+    function memoryStorageLocation(Pointer ptr) internal pure returns (Pointer result) {
+        result = Pointer.wrap(MemoryLib.memoryStorageLocation(Pointer.unwrap(ptr)));
     }
 }
