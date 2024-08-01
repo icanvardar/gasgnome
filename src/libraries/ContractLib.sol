6535b42e8a2e7fec40a68200c1d411adf2c65ff4
Can Vardar <ismailcanvardar@gmail.com>
2024-08-01 15:48:49 +0300

    feat(ContractLib): implement delegatecall

--- src/libraries/ContractLib.sol
+++ src/libraries/ContractLib.sol
@@ -221,6 +221,81 @@ library ContractLib {
         }
     }
 
+    /// delegatecall function + get return value
+    function delegatecall(
+        Contract c,
+        FunctionSignature sig,
+        bool hasOutput
+    )
+        internal
+        view
+        returns (bytes memory result)
+    {
+        assembly {
+            let ptr := mload(0x40)
+            mstore(ptr, sig)
+
+            let success := staticcall(gas(), c, ptr, 0x04, 0x00, 0x00)
+
+            if iszero(success) {
+                /// @dev bytes4(keccak256("UnableToCall()")) => 0x09108f6e
+                mstore(0x80, 0x09108f6e)
+                revert(0x9c, 0x04)
+            }
+
+            if eq(hasOutput, 0x1) {
+                let size := returndatasize()
+                mstore(ptr, size)
+                returndatacopy(add(ptr, 0x20), 0x0, size)
+                mstore(0x40, add(ptr, add(size, 0x20)))
+
+                result := ptr
+            }
+        }
+    }
+
+    /// delegatecall function + get return value + with input
+    function delegatecall(
+        Contract c,
+        FunctionSignature sig,
+        FunctionInput[] memory input,
+        bool hasOutput
+    )
+        internal
+        view
+        returns (bytes memory result)
+    {
+        assembly {
+            let inputLen := mload(input)
+            let ptr := mload(0x40)
+            mstore(ptr, sig)
+            let nextPtr := add(ptr, 0x04)
+
+            let i
+            for { i := 0 } lt(i, inputLen) { i := add(i, 0x1) } {
+                let multiplier := mul(i, 0x20)
+                mstore(add(nextPtr, multiplier), mload(add(input, add(multiplier, 0x20))))
+            }
+
+            let success := staticcall(gas(), c, ptr, add(0x04, mul(inputLen, 0x20)), 0x00, 0x00)
+
+            if iszero(success) {
+                /// @dev bytes4(keccak256("UnableToCall()")) => 0x09108f6e
+                mstore(0x80, 0x09108f6e)
+                revert(0x9c, 0x04)
+            }
+
+            if eq(hasOutput, 0x1) {
+                let size := returndatasize()
+                mstore(ptr, size)
+                returndatacopy(add(ptr, 0x20), 0x0, size)
+                mstore(0x40, add(ptr, add(size, 0x20)))
+
+                result := ptr
+            }
+        }
+    }
+
     function balance(Contract c) public view returns (uint256 result) {
         assembly {
             result := balance(c)
