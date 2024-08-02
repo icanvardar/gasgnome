987dca386015e231e03e17552fe5e4b649e20db8
Can Vardar <ismailcanvardar@gmail.com>
2024-08-02 03:23:35 +0300

    chore(ContractLibProxy): add logic slots

--- test/mocks/ContractLibProxy.sol
+++ test/mocks/ContractLibProxy.sol
@@ -4,6 +4,10 @@ pragma solidity 0.8.26;
 import { Contract, ContractLib, FunctionInput, FunctionSignature } from "../../src/libraries/ContractLib.sol";
 
 contract ContractLibProxy {
+    bytes32 internal numberOneSlot = bytes32(keccak256("number.1"));
+    bytes32 internal numberTwoSlot = bytes32(keccak256("number.2"));
+    bytes32 internal numberThreeSlot = bytes32(keccak256("number.3"));
+    bytes32 internal numberFourSlot = bytes32(keccak256("number.4"));
     bytes32 internal implementationSlot = bytes32(keccak256("implementation"));
 
     constructor(address _implementation) {
@@ -18,21 +22,45 @@ contract ContractLibProxy {
         }
     }
 
-    function delegatecallWithoutInputAndWithoutReturnValue() public view {
+    function getNumberOne() public view returns (uint256 result) {
+        assembly {
+            result := sload(numberOneSlot.slot)
+        }
+    }
+
+    function getNumberTwo() public view returns (uint256 result) {
+        assembly {
+            result := sload(numberTwoSlot.slot)
+        }
+    }
+
+    function getNumberThree() public view returns (uint256 result) {
+        assembly {
+            result := sload(numberThreeSlot.slot)
+        }
+    }
+
+    function getNumberFour() public view returns (uint256 result) {
+        assembly {
+            result := sload(numberFourSlot.slot)
+        }
+    }
+
+    function delegatecallWithoutInputAndWithoutReturnValue() public {
         Contract c = getImplementationContract();
         FunctionSignature fs = FunctionSignature.wrap(bytes4(keccak256("setNumberOne()")));
 
         c.delegatecall(fs, false);
     }
 
-    function delegatecallWithoutInputAndWithReturnValue() public view returns (bytes memory result) {
+    function delegatecallWithoutInputAndWithReturnValue() public returns (bytes memory result) {
         Contract c = getImplementationContract();
         FunctionSignature fs = FunctionSignature.wrap(bytes4(keccak256("setNumberTwo()")));
 
         result = c.delegatecall(fs, true);
     }
 
-    function delegatecallWithInputAndWithoutReturnValue() public view {
+    function delegatecallWithInputAndWithoutReturnValue() public {
         Contract c = getImplementationContract();
         FunctionSignature fs = FunctionSignature.wrap(bytes4(keccak256("setNumberThree(uint256)")));
         FunctionInput[] memory fi = new FunctionInput[](1);
@@ -41,7 +69,7 @@ contract ContractLibProxy {
         c.delegatecall(fs, fi, false);
     }
 
-    function delegatecallWithInputAndWithReturnValue() public view returns (bytes memory result) {
+    function delegatecallWithInputAndWithReturnValue() public returns (bytes memory result) {
         Contract c = getImplementationContract();
         FunctionSignature fs = FunctionSignature.wrap(bytes4(keccak256("setNumberFour(uint256)")));
         FunctionInput[] memory fi = new FunctionInput[](1);
